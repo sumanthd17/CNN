c7f6935253fab2c28b22641277f5f2dfe9041d9a
Sumanth <dsumanth17@gmail.com>
2019-08-05 22:47:59 +0530

    back prop

--- ops.py
+++ ops.py
@@ -49,3 +49,47 @@ def maxpool(image, mask, stride):
 def softmax(x):
 	out = np.exp(x)
 	return out / np.sum(out)
+
+def convolutionBackward(dconv_prev, conv_in, filt, s):
+	(n_f, n_c, f, _) = filt.shape
+	(_, org_dim, _) = conv_in.shape
+
+	dout = np.zeros(conv_in.shape)
+	dfilt = np.zeros(filt.shape)
+	dbias = np.zeros((n_f, 1))
+
+	for curr_f in range(n_f):
+		curr_y = out_y = 0
+		while curr_y + f <= org_dim:
+			curr_x = out_x = 0
+			while curr_x + f <= org_dim:
+				dfilt[curr_f] += dconv_prev[curr_f, out_y, out_x] * conv_in[:, curr_y:curr_y+f, curr_x: curr_x+f]
+				dout[:, curr_y: curr_y+f, curr_x:curr_x+f] += dconv_prev[curr_f, out_y, out_x] * filt[curr_f]
+				curr_x += s
+				out_x += 1
+			curr_y += s
+			out_y += 1
+
+		dbias[curr_f] = np.sum(dconv_prev[curr_f])
+
+	return dout, dfilt, dbias
+
+def maxpoolBackward(dpool, org, f, s):
+	(n_c, org_dim, _) = org.shape
+
+	dout = np.zeros(org.shape)
+
+	for curr_c in range(n_c):
+		curr_y = out_y = 0
+		while curr_y +f <= org_dim:
+			curr_x = out_x = 0
+			while curr_x + f <= org_dim:
+				(a, b) = nanargmax(org[curr_c, curr_y: curr_y+f, curr_x: curr_x+f])
+				dout[curr_c, curr_y+a, curr_x+b] = dpool[curr_c, out_y, out_x]
+
+				curr_x += s
+				out_x += 1
+			curr_y += s
+			out_y += 1
+
+		return dout
